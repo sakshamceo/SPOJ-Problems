851142e6e804bcf80af160948576d1681c2b4389
saksham garg <59284238+sakshamceo@users.noreply.github.com>
2021-06-11 10:44:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+for(int i=0;;i++)
+{
+    cin>>n;
+    if(n==42)
+    {
+        break;
+    }
+    cout<<n<<endl;
+}
+}
+//SakshamGarg
